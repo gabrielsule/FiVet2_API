055acdf7c03855d2db2991b5f94d2f0fdcf38324
Gabriel D. Sulé <gabrielsule@gmail.com>
2017-03-31 00:33:39 -0300

    Revert "Version 2"
    This reverts commit d813faa02e1d6de8f21af4ee69d7682df87115dc.

--- Fivet2Api/Areas/HelpPage/ModelDescriptions/CollectionModelDescription.cs
+++
@@ -1,7 +0,0 @@
-namespace Fivet2Api.Areas.HelpPage.ModelDescriptions
-{
-    public class CollectionModelDescription : ModelDescription
-    {
-        public ModelDescription ElementDescription { get; set; }
-    }
-}
--- Fivet2Api/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
+++
@@ -1,6 +0,0 @@
-namespace Fivet2Api.Areas.HelpPage.ModelDescriptions
-{
-    public class DictionaryModelDescription : KeyValuePairModelDescription
-    {
-    }
-}
